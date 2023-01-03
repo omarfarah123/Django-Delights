bd2cb68b34764d1b410e7cfbcc84028ccafef988
Omar Farah <cronogy@gmail.com>
2023-01-03 11:25:03 -0500

    Created admin.py and registered models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredient, MenuItem, RecipieRequirment, Purchase
+
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(RecipieRequirment)
+admin.site.register(Purchase)
