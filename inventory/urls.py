fd5b9d11a2295784622994fa69224dd7d419fbc3
Omar Farah <cronogy@gmail.com>
2023-01-06 18:35:27 -0500

    Added urls for all view classes and methods

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,5 +3,22 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.HomeView.as_view(), name='home'),
+    path('ingredients/', views.IngredientListView.as_view(), name='ingredients'),
+    path('ingredients/new', views.IngredientCreateView.as_view(), name='add_ingredient'),
+    path('ingredients/<pk>/update', views.IngredientUpdateView.as_view(), name='update_ingredient'),
+    path('ingredients/<pk>/delete', views.IngredientDeleteView.as_view(), name='delete_ingredient'),
+    path('menuitem/', views.MenuItem.as_view(), name='menuitems'),
+    path('menuitem/new', views.MenuItemCreateView.as_view(), name='add_menuitem'),
+    path('menuitem/<pk>/update', views.MenuItemUpdateView.as_view(), name='update_menuitem'),
+    path('menuitem/<pk>/delete', views.MenuItemDeleteView.as_view(), name='delete_menuitem'),
+    path('purchases/', views.PurchaseListView.as_view(), name='purchases'),
+    path('purchases/new', views.PurchaseCreateView.as_view(), name='add_purchase'),
+    path('purchases/<pk>/update', views.PurchaseUpdateView.as_view(), name='update_purchase'),
+    path('purchases/<pk>/delete', views.PurchaseDeleteView.as_view(), name='delete_purchase'),
+    path('reciperequirments/', views.RecipeRequirmentListView.as_view(), name="reciperequirments"),
+    path('reciperequirments/new', views.RecipeRequirmentCreateView.as_view(), name="add_reciperequirment"),
+    path('reciperequirments/<pk>/update', views.RecipeRequirmentUpdateView.as_view(), name="update_reciperequirment"),
+    path('reciperequirments<pk>/delete', views.RecipeRequirmentDeleteView.as_view(), name="delete_reciperequirment"),
+    path('finances/', views.profit_vs_revenue.as_view(), name='finances')
 ]
