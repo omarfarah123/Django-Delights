9f31e443a2f33c3c59bc689f960834c9b875f493
Omar Farah <cronogy@gmail.com>
2023-01-05 22:11:26 -0500

    Created Views

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,102 @@
 from django.shortcuts import render
-
+from .models import Ingredient, MenuItem, RecipeRequirment, Purchase
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import *
 # Create your views here.
+class HomeView(TemplateView): 
+    template_name = "inventory/home.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["Ingredient"] = Ingredient.objects.all()
+        context["MenuItem"] = MenuItem.objects.all()
+        context["RecipeRequirment"] = RecipeRequirment.objects.all()
+        return context
+
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = "inventory/ingredients.html"
+
+class IngredientsCreate(CreateView):
+    model = Ingredient
+    template_name = "inventory/add_ingredient.html"
+    form_class = IngredientCreateForm
+
+class IngredientUpdateView(UpdateView):
+    model = Ingredient
+    template_name = "inventory/update_ingredient.html"
+    form_class = IngredientUpdateForm
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    template_name = "inventory/delete_ingredient.html"
+    success_url = "inventory/ingredients.html"
+
+class MenuItemView(ListView):
+    model = MenuItem
+    template_name = "inventory/menu.html"
+
+class MenuItemCreate(CreateView):
+    model = MenuItem
+    template_name = "inventory/add_menu_item.html"
+    form_class = MenuItemCreateForm
+
+class MenuItemUpdate(UpdateView):
+    model = MenuItem
+    template_name = "inventory/update_menu_item.html"
+    form_class = MenuItemUpdateForm
+
+class MenuItemDelete(DeleteView):
+    model = MenuItem
+    template_name = "inventory/delete_menu_item.html"
+    success_url = "inventory/menu.html"
+
+class RecipeRequirmentsView(ListView):
+    model = RecipeRequirment
+    template_name = "inventory/recipie_requirments.html"
+
+class RecipeRequirmentCreate(CreateView):
+    model = RecipeRequirment
+    template_name = "inventory/add_recipe_requirment.html"
+    form_class = RecipieRequirmentCreateForm
+
+class RecipeRequirmentUpdate(UpdateView):
+    model = RecipeRequirment
+    template_name = "inventory/update_recipe_requirment.html"
+    form_class = RecipieRequirmentUpdateForm
+
+class RecipeRequirmentDelete(DeleteView):
+    model = RecipeRequirment
+    template_name = "inventory/delete_recipe_requirment.html"
+
+class PurchaseList(ListView):
+    model = Purchase
+    template_name = "inventory/purchases.html"
+
+class PurchaseCreate(CreateView):
+    model = Purchase
+    template_name = "inventory/add_purchase.html"
+    form_class = PurchaseCreateForm
+
+class PurchaseUpdate(UpdateView):
+    model = Purchase
+    template_name = "inventory/update_purchase.html"
+    form_class = PurchaseUpdateForm
+
+class PurchaseDelete(DeleteView):
+    model = Purchase
+    template_name = "inventory/delete_purchase.html"
+    success_url = "inventory/purchases.html"
+
+def profit_vs_revenue(request):
+    revenue = 0
+    purchase = Purchase.objects.all()
+    for item in purchase:
+        revenue += item.menu_item.price
+    context = {
+        "revenue": revenue
+    }
+    return render(request, 'inventory/profitvsrevenue.html', context)
+
+
