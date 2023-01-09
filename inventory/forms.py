7a682b8cc8ff3eb0006ae75519acd6256cd1abfb
Omar Farah <cronogy@gmail.com>
2023-01-09 12:04:54 -0500

    Added meta class for model refrence

--- inventory/forms.py
+++ inventory/forms.py
@@ -24,20 +24,24 @@ class MenuItemUpdateForm(forms.ModelForm):
         model = MenuItem
         fields = "__all__"
 
-class RecipieRequirmentCreateForm(forms.ModelForm):
-    model = RecipeRequirment
-    fields = "__all__"
+class RecipeRequirmentCreateForm(forms.ModelForm):
+    class Meta:
+        model = RecipeRequirment
+        fields = "__all__"
 
-class RecipieRequirmentUpdateForm(forms.ModelForm):
-    model = RecipeRequirment
-    fields = "__all__"
+class RecipeRequirmentUpdateForm(forms.ModelForm):
+    class Meta:
+        model = RecipeRequirment
+        fields = "__all__"
 
 
 class PurchaseCreateForm(forms.ModelForm):
-    model = Purchase
-    fields = "__all__"
+    class Meta:
+        model = Purchase
+        fields = "__all__"
 
 class PurchaseUpdateForm(forms.ModelForm):
-    model = Purchase
-    fields = "__all__"
+    class Meta:
+        model = Purchase
+        fields = "__all__"
 
